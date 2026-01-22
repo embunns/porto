0bdae27dded8d948fac804c2eb44382db83402d7
Embun Nawang Sari <embunnawangsari@gmail.com>
2026-01-22 23:54:08 +0700

    add new project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio - Your Name",
+  title: "Portfolio - Embun",
   description: "My personal portfolio website",
 };
 
